faecb6b512386a84bc2f83058368428844bd9cdd
Paulo Serrano <pvsserrano@gmail.com>
2022-06-29 11:22:54 -0300

    função openQuiz criada

--- script/main.js
+++ script/main.js
@@ -1,8 +1,8 @@
-const apiURL = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
+const apiURL = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/"
 
 
-const getQuizzes = () => {
-    axios.get(apiURL)
+const getQuizzes = (quizId='') => {
+    axios.get(`${apiURL}/${quizId}`)
         .catch(err => console.log(err))
         .then(response => renderQuizzes(response.data))
 }
@@ -12,17 +12,23 @@ const renderQuizzes = (quizzes) => {
     const allQuizzesContainer = document.querySelector('.all-quizzes .quizzes-container')
 
     quizzes.forEach(quiz => {
-        allQuizzesContainer.innerHTML += `<li data-quiz-id="${quiz.id}">${quiz.title}</li>` 
+        allQuizzesContainer.innerHTML += `<li data-id="${quiz.id}">${quiz.title}</li>` 
 
         const newQuiz = allQuizzesContainer.querySelector(':last-child')
         newQuiz.style.backgroundImage = `url(${quiz.image})`
     });
 
+        
+    const allQuizzes = allQuizzesContainer.querySelectorAll('li')
+    allQuizzes.forEach(quiz => quiz.addEventListener('click', e => openQuiz(e.target)))
+
     console.log(quizzes)
 }
 
 
-const openQuiz = () => {
+const openQuiz = (e) => {
+    const quizId = e.dataset.id
+    console.log(quizId)
     /* TO-DO */
 }
 
